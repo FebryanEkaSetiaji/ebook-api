c2d20d399bc5fdac8f58790ab743d4b4d7f84b6e
FebryanEkaSetiaji <febryaneka528@gmail.com>
2021-01-10 18:22:17 +0700

    book

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,9 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('books', 'BookController@index');
+Route::post('books', 'BookController@store');
+Route::get('books/{id}', 'BookController@show');
+Route::put('books/edit/{id}', 'BookController@update');
+Route::delete('books/delete/{id}', 'BookController@destroy');
